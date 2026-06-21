7d9827d2df7c94ff812c6c96947d94cf14bf101b
MaxDustD <patojubileu987@gmail.com>
2026-06-21 15:46:20 -0300

    feat: adição do código na classe movimentação

--- SistemaGestaoOcorrencias.Domain/Entities/Movimentacao.cs
+++ SistemaGestaoOcorrencias.Domain/Entities/Movimentacao.cs
@@ -1,6 +1,17 @@
+using SistemaGestaoOcorrencias.Domain.Enums;
+
 namespace SistemaGestaoOcorrencias.Domain.Entities;
 
 public class Movimentacao
 {
-    
+    public TipoMovimentacao Tipo {get;}
+    public DateTime Data {get;}
+    public string? Descricao {get;}
+
+    public Movimentacao (TipoMovimentacao tipo, DateTime data, string? descricao)
+    {
+        Tipo = tipo;
+        Data = data;
+        Descricao = descricao;
+    }
 }
