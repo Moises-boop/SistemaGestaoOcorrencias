23758a66cf6b83598e94cfe3296a2e7e6f10d8a5
MaxDustD <patojubileu987@gmail.com>
2026-06-21 09:18:31 -0300

    feat: adição do código da classe impedimento

--- SistemaGestaoOcorrencias.Domain/Entities/Impedimento.cs
+++ SistemaGestaoOcorrencias.Domain/Entities/Impedimento.cs
@@ -2,5 +2,33 @@ namespace SistemaGestaoOcorrencias.Domain.Entities;
 
 public class Impedimento
 {
-    
+    public string Tipo {get;}
+    public DateTime DataRegistro {get;}
+    public bool Resolvido {get; private set;}
+    public DateTime? DataResolucao {get; private set;}
+
+    public Impedimento (string tipo)
+    {
+        Tipo = string.IsNullOrEmpty(tipo) ? throw new ArgumentException ("O tipo do impedimento não pode ser nulo em branco") : tipo;
+        DataRegistro = DateTime.Now();
+        Resolvido = false;
+    }
+
+    public void Resolver()
+    {
+        if (Resolvido == true)
+            throw new ArgumentException ("O impedimento já foi resolvido");
+        Resolvido = true;
+        DataResolucao = DateTime.Now();
+    }
+
+    public bool ImpedimentoAtivo()
+    {
+        if (Resolvido)
+            return false;
+            
+        return true;
+    }
+
+
 }
