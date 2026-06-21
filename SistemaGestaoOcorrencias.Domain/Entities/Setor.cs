f40f8ddf6c59f6425e50ae3e7bf776159712259b
Moises Grassi <moisesgrassi@alunos.utfpr.edu.br>
2026-06-21 17:53:43 -0300

    Feat: Adicionada as classes Movimentacao e Setor.

--- SistemaGestaoOcorrencias.Domain/Entities/Setor.cs
+++ SistemaGestaoOcorrencias.Domain/Entities/Setor.cs
@@ -2,5 +2,14 @@ namespace SistemaGestaoOcorrencias.Domain.Entities;
 
 public class Setor
 {
-    
+    public string IdSetor { get; }
+    public string Nome { get; }
+    public string Descricao { get; }
+
+    public Setor(string idSetor, string nome, string descricao)
+    {
+        IdSetor = Validacao.ValidarTexto(idSetor, "ID do setor é obrigatório.");
+        Nome = Validacao.ValidarTexto(nome, "Nome do setor é obrigatório.");
+        Descricao = Validacao.ValidarTexto(descricao, "Descrição do setor é obrigatória.");
+    }
 }
